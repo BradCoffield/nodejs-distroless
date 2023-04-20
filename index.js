c60a6b2953032385f2263b95bc21d0692b2126fd
Brad <bcoffield@gmail.com>
2023-04-20 16:15:03 -0600

    init fastify?

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const http = require('http');
-const PORT = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+// CommonJs
+const fastify = require("fastify")({
+  logger: true,
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+fastify.get("/", async (request, reply) => {
+  return { hello: "world" };
 });
+
+/**
+ * Run the server!
+ */
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
